cedbf1c62af63d3f71cc9b982796a61ab9fa66d1
saitarun1806 <kunanisai123@gmail.com>
2026-07-11 20:22:33 +0530

    server & client

--- server/src/routes/feedbackRoutes.js
+++ server/src/routes/feedbackRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 
-import {submitFeedback, getAllFeedback, getFeedbackByComplaintId,getFeedbackByAgent} from "../controllers/feedbackController.js";
-import {auth, adminOnly } from "../middleware/authMiddleware.js";
+import { submitFeedback, getAllFeedback, getFeedbackByComplaintId, getFeedbackByAgent } from "../controllers/feedbackController.js";
+import { auth, adminOnly, agentOrAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/:complaintId', auth, submitFeedback);
+router.get('/agent/:agentId', auth, agentOrAdmin, getFeedbackByAgent);   
 router.get('/:complaintId', auth, getFeedbackByComplaintId);
-router.get('/agent/:agentId', auth, getFeedbackByAgent);
 router.get('/', auth, adminOnly, getAllFeedback);
 
 export default router;
